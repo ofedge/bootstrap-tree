5813205db74f34b1c73cde1bc7fbc0ae36b0467f
ofedge <silcata@163.com>
2015-07-12 23:10:54 +0800

    beta 2.0!!! 功能不变, 代码重构

--- src/js/bootstrap-tree.js
+++ src/js/bootstrap-tree.js
@@ -1,106 +1,97 @@
 (function($){
-  // add node click function (private)
-  var clickBtn = function(){
-    if (isClickBtn) return;
-    var icons = $.fn.bootstrapTree.settings.icons;
-    var openedNodeIcon = icons.openedNodeIcon;
-    var closedNodeIcon = icons.closedNodeIcon;
-    $(this).find('li.parent_li > span').on('click', function(e) {
-      var children = $(this).parent('li.parent_li').find(' > ul > li');
-      if (children.is(":visible")) {
-        children.hide('fast');
-        $(this).attr('node-status', 'closed').find(' > span').removeClass(openedNodeIcon).addClass(closedNodeIcon);
-      } else {
-        children.show('fast');
-        $(this).attr('node-status', 'open').find(' > span').removeClass(closedNodeIcon).addClass(openedNodeIcon);
-      };
-      e.stopPropagation();
-    });
-    isClickBtn = true;
-  };
-  // functions
-  var method = {
-      closeAll: function() {
-        return $(this).each(function(){
-          $(this).find('li.parent_li > span').each(function(){
-            if($(this).parent('li.parent_li').find(' > ul > li').is(':visible'))
-              $(this).trigger('click', $.fn.bootstrapTree.settings.icons);
-          });
-        });
-      },
-      openAll: function() {
-        return $(this).each(function(){
-          $(this).find('li.parent_li > span').each(function(){
-            if($(this).parent('li.parent_li').find(' > ul > li').is(':hidden'))
-              $(this).trigger('click', $.fn.bootstrapTree.settings.icons);
-          });
-        });
-      },
-      init: function() {
-        isClickBtn = false;
-        return $(this).each(function(){
-          clickBtn.apply(this);
-        });
-      },
-      destroy: function() {
-        return $(this).each(function(){
-          $(this).find('li.parent_li > span').off('click');
-        });
-      }
-  };
-  
-  // init mark (private)
-  var isInit = false;
-  // click button mark (private)
-  var isClickBtn = false;
-  
-  // init (private)
-  var init = function(){
-    var icons = $.fn.bootstrapTree.settings.icons;
-    var openedNodeIcon = icons.openedNodeIcon;
-    var leafNodeIcon = icons.leafNodeIcon;
-    $(this).find('li:has(ul)').addClass('parent_li').find(' > span').attr('node-status', 'open');
-    $(this).find('li:has(ul)').find(' > span').prepend('<span class="glyphicon ' + openedNodeIcon + '" aria-hidden="true"></span>');
-    $(this).find('li[class!=parent_li]').find(' > span').prepend('<span class="glyphicon ' + leafNodeIcon + '" aria-hidden="true"></span>');
-    isInit = true;
-  };
-  
-  $.fn.bootstrapTree = function(options) {
-    if (!isInit) {
-      $.fn.bootstrapTree.settings = (typeof options == 'object') ? $.extend(true, {}, $.fn.bootstrapTree.defaults, options) : $.fn.bootstrapTree.defaults;
-      $(this).addClass('tree');
-      init.apply(this);
-    };
-    clickBtn.apply(this);
-    var settings = $.fn.bootstrapTree.settings;
-    return this.each(function(){
-      var $this = $(this);
-      // when event
-      if (typeof options == 'string') {
-        return method[options].apply(this);
-      };
-      // when parameter
-      if (typeof options == 'object') {
-        //icon
-        var openedNodeIcon = settings.icons.openedNodeIcon;
-        var closedNodeIcon = settings.icons.closedNodeIcon;
-        var leafNodeIcon = settings.icons.leafNodeIcon;
-        // openOnLoad
-        if(!settings.openOnLoad) {
-          $(this).find('li.parent_li > span').attr('node-status', 'closed').find(' > span').removeClass(openedNodeIcon).addClass(closedNodeIcon);
-          $(this).find('li.parent_li').find(' > ul > li').hide();
-        };
-      };
-    });
-  };
-  // default parameter
-  $.fn.bootstrapTree.defaults = {
-      bootstrapversion: 3,
-      openOnLoad: true,
-      icons: {
-        openedNodeIcon: 'glyphicon-minus',
-        closedNodeIcon: 'glyphicon-plus',
-        leafNodeIcon: 'glyphicon-leaf'
-      }
-  };
+	// 定义 BootstrapTree 构造函数
+	var BootstrapTree = function(element, options) {
+		this.element = element;
+		this.defaults = {
+			openOnLoad: false,
+			icons: {
+				openedNodeIcon: 'glyphicon-minus',
+				closedNodeIcon: 'glyphicon-plus',
+				leafNodeIcon: 'glyphicon-leaf'
+			}
+		};
+		this.settings = $.extend(true, {}, this.defaults, options); // 初始化设置
+		this.initialize(); // 调用开始
+	};
+	// 定义原型方法 prototype function
+	BootstrapTree.prototype = {
+			initialize: function() {
+				this.$element = $(this.element);
+				this.$element.addClass('tree');
+				var icons = this.settings.icons;
+				var openedNodeIcon = icons.openedNodeIcon;
+				var leafNodeIcon = icons.leafNodeIcon;
+				this.$element.find('li:has(ul)').addClass('parent_li').find(' > span').attr('node-status', 'open');
+				this.$element.find('li:has(ul)').find(' > span').prepend('<span class="glyphicon ' + openedNodeIcon + '" aria-hidden="true"></span>');
+				this.$element.find('li[class!=parent_li]').find(' > span').prepend('<span class="glyphicon ' + leafNodeIcon + '" aria-hidden="true"></span>');
+				this.bindClick();
+			},
+			bindClick: function() {
+				var icons = this.settings.icons;
+				var openedNodeIcon = icons.openedNodeIcon;
+				var closedNodeIcon = icons.closedNodeIcon;
+				this.$element.find('li.parent_li > span').on('click', function(e) {
+					var children = $(this).parent('li.parent_li').find(' > ul > li');
+					if (children.is(":visible")) {
+						children.hide('fast');
+						$(this).attr('node-status', 'closed').find(' > span').removeClass(openedNodeIcon).addClass(closedNodeIcon);
+					} else {
+						children.show('fast');
+						$(this).attr('node-status', 'open').find(' > span').removeClass(closedNodeIcon).addClass(openedNodeIcon);
+					};
+					e.stopPropagation();
+				});
+			},
+			setOptions: function(options){ // 设置参数
+				var settings = this.settings;
+				if(!settings.openOnLoad) {
+					this.$element.find('li.parent_li > span').attr('node-status', 'closed').find(' > span').removeClass(settings.icons.openedNodeIcon).addClass(settings.icons.closedNodeIcon);
+					this.$element.find('li.parent_li').find(' > ul > li').hide();
+				};
+				return this.$element;
+			},
+			closeAll: function() {
+				var settings = this.settings;
+				return this.$element.find('li.parent_li > span').each(function(){
+					if($(this).parent('li.parent_li').find(' > ul > li').is(':visible'))
+						$(this).trigger('click', settings.icons);
+				});
+			},
+			openAll: function() {
+				var settings = this.settings;
+				return this.$element.find('li.parent_li > span').each(function(){
+					if($(this).parent('li.parent_li').find(' > ul > li').is(':hidden'))
+						$(this).trigger('click', settings.icons);
+				});
+			},
+			init: function() {
+				return this.bindClick();
+			},
+			destroy: function() {
+				return this.$element.find('li.parent_li > span').off('click');
+			}
+	};
+	$.fn.bootstrapTree = function(options) {
+		var args = arguments;
+		result = null;
+		return $(this).each(function(index, dom){
+			var $this = $(dom);
+			data = $this.data('bootstrapTree'); // 通过 data 来判断是否已初始化
+			if(!data){
+				data = new BootstrapTree(dom, options);
+				$(data.$element).data('bootstrapTree', data);
+			};
+			if(typeof options == 'string'){
+				if(data[options]){
+					result = data[options].apply(data, Array.prototype.slice.call(args, 1));
+				}else{
+					throw 'Method "' + options + '" does not exists.';
+				}
+			}else{
+				result = data.setOptions(options);
+			}
+			return result;
+		});
+	};
+	$.fn.bootstrapTree.Constructor = BootstrapTree;
 })(window.jQuery);
